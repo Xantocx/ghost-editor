48b2a2078274b271a8b3374f7210b3d6a429c827
Maximilian Mayer <maximilian_mayer@gmx.net>
2023-04-28 19:09:48 +0200

    backup while buidling versioning prototype

--- vcs-test.ts
+++ vcs-test.ts
@@ -3,27 +3,198 @@ import { VCSSnapshotData, VCSSnapshot } from "./src/app/components/data/snapshot
 import { IRange } from "./src/app/components/utils/range";
 import { VCSServer } from "./src/app/components/vcs/vcs-provider";
 
+class TrackedBlock {
 
-class TrackedFile {
+    protected eol: string
 
-    private filePath: string | null
-    private lines: TrackedLine[]
-    private snapshots: Snapshot[]
+    protected firstLine: TrackedLine | undefined
+    protected lastLine:  TrackedLine | undefined
 
-    public static create(filePath: string | null, content: string, eol: string): TrackedFile {
-        const file
+    constructor(eol: string, firstLine?: TrackedLine | undefined, lastLine?: TrackedLine | undefined) {
+        this.eol = eol
+        this.firstLine = firstLine
+        this.lastLine  = lastLine
+    }
 
-        const lines = content.split(eol)
-        const trackedLines = lines.map(line => {
-            return new TrackedLine(file, lineNumber, timestamp, line)
+    public get lineCount(): number {
+        return this.lines.length
+    }
+
+    public get lines(): TrackedLine[] {
+
+        const lines = []
+        let   line  = this.firstLine
+
+        while (line !== this.lastLine) {
+            lines.push(line)
+            line = line.next
+        }
+
+        return lines
+    }
+
+    protected set lines(lines: TrackedLine[]) {
+        const lineCount = lines.length
+
+        if (lineCount === 0) {
+            this.firstLine = undefined
+            this.lastLine  = undefined
+        } else {
+            this.firstLine = lines[0]
+            this.lastLine  = lines[lineCount - 1]
+
+            let previous: TrackedLine | undefined = undefined
+            for (let i = 0; i <= lineCount; i++) {
+                const current = lines[i]
+
+                current.previous = previous
+                if (i < lineCount - 1) { current.next = lines[i + 1] }
+
+                previous = current
+            }
+        }
+    }
+
+    public get stringLines(): string[] {
+        return this.lines.map(line => {
+            return line.currentContent
         })
     }
 
-    constructor(filePath: string | null, lines?: TrackedLine[], snapshots?: Snapshot[]) {
-        this.filePath = filePath
-        this.lines = lines ? lines : []
-        this.snapshots = snapshots ? snapshots : []
+    public get currentText(): string {
+        return this.stringLines.join(this.eol)
     }
+
+    public getLine(lineNumber: number): TrackedLine | null {
+        const lineCount = this.lineCount
+        if (lineNumber < 1 || lineNumber > lineCount) { return null }
+
+        let line = this.firstLine
+        for (let i = 1; i < lineNumber; i++) {
+            line = line.next
+        }
+
+        return line
+    }
+}
+
+class ArchivedBlock extends TrackedBlock {
+
+    // used to determine original location in the code
+    public readonly previous: TrackedLine | undefined
+    public readonly next: TrackedLine | undefined
+
+    constructor(eol: string, firstLine: TrackedLine, lastLine: TrackedLine, relations?: TrackedLineRelation) {
+        super(eol, firstLine, lastLine)
+
+        if (relations) {
+            this.previous = relations.previous
+            this.next = relations.next
+        }
+    }
+}
+
+interface LineRange {
+    start: number,
+    end: number
+}
+
+class TrackedFile extends TrackedBlock {
+
+    public static create(filePath: string | null, eol: string, content: string): TrackedFile {
+        const file = new TrackedFile(filePath, eol)
+
+        const lines = content.split(eol)
+        const timestamp = file.getTimestamp()
+
+        const trackedLines = lines.map(line => {
+            return new TrackedLine(file, timestamp, line)
+        })
+
+        file.lines = trackedLines
+        console.log(file.currentText)
+
+        return file
+    }
+
+    private filePath: string | null
+    private archivedBlocks: ArchivedBlock[] = []
+    private snapshots: Snapshot[] = []
+
+    constructor(filePath: string | null, eol: string) {
+        super(eol)
+        this.filePath = filePath
+    }
+
+    private nextTimestamp = 0
+    public getTimestamp(): number {
+        const timestamp = this.nextTimestamp
+        this.nextTimestamp++
+        return timestamp
+    }
+
+    public insertLine(lineNumber: number, content: string): TrackedLine {
+        const newLastLine = this.lineCount + 1
+        const adjustedLineNumber = Math.min(Math.max(lineNumber, 1), newLastLine)
+        const timestamp = this.getTimestamp()
+
+        if (adjustedLineNumber === 1) {
+            this.firstLine = new TrackedLine(this, timestamp, content, { next:    this.firstLine })
+            return this.firstLine
+        } else if (adjustedLineNumber === newLastLine) {
+            this.lastLine  = new TrackedLine(this, timestamp, content, { previous: this.lastLine })
+            return this.lastLine
+        } else {
+            const currentLine = this.getLine(adjustedLineNumber)
+            const newLine  = new TrackedLine(this, timestamp, content, { 
+                previous: currentLine.previous, 
+                next: currentLine 
+            })
+            return newLine
+        }
+    }
+
+    public insertLines(lineNumber: number, contents: string[]): TrackedLine[] {
+        let offset = 0
+        return contents.map(line => {
+            return this.insertLine(lineNumber + offset++, line)
+        })
+    }
+
+    public deleteLine(lineNumber: number): ArchivedBlock {
+        return this.deleteLines({ start: lineNumber, end: lineNumber })
+    }
+
+    public deleteLines(range: LineRange): ArchivedBlock {
+        if (range.start > range.end) { throw new Error(`Start of range cannot be smaller than end: ${range}`) }
+
+        const start = this.getLine(range.start)
+        const end   = this.getLine(range.end)
+
+        if (!start || !end) { throw new Error(`Cannot delete invalid range ${range}!`) }
+
+        const previous = start.previous
+        const next     = end.next
+
+        const archivedBlock = new ArchivedBlock(this.eol, start, end, {
+            previous: previous,
+            next: next
+        })
+
+        if (previous) { previous.next = next }
+        if (next)     { next.previous = previous }
+
+        start.previous = undefined
+        end.next       = undefined
+
+        this.archivedBlocks.push(archivedBlock)
+        return archivedBlock
+    }
+}
+
+interface TrackedLineRelation {
+    previous?: TrackedLine | undefined
+    next?: TrackedLine | undefined
 }
 
 class TrackedLine {
@@ -31,13 +202,32 @@ class TrackedLine {
     public readonly file: TrackedFile
     public readonly history: LineHistory
 
-    private lineNumber: number
+    public previous: TrackedLine | undefined
+    public next: TrackedLine | undefined
 
-    constructor(file: TrackedFile, lineNumber: number, initialTimestamp: number, initialContent: string) {
+    public get lineNumber() {
+        if (this.previous) {
+            return this.previous.lineNumber + 1
+        } else {
+            return 1
+        }
+    }
+
+    public get currentContent(): string {
+        return this.history.currentContent
+    }
+
+    constructor(file: TrackedFile, initialTimestamp: number, initialContent: string, relations?: TrackedLineRelation) {
         this.file = file
         this.history = new LineHistory(this, initialTimestamp, initialContent)
 
-        this.lineNumber = lineNumber
+        if (relations) {
+            this.previous = relations.previous
+            this.next = relations.next
+
+            if (this.previous) { this.previous.next = this }
+            if (this.next)     { this.next.previous = this }
+        }
     }
 }
 
@@ -50,9 +240,13 @@ class LineHistory {
     private start: LineVersion
     private end: LineVersion
 
+    public get currentContent(): string {
+        return this.head.content
+    }
+
     constructor(line: TrackedLine, initialTimestamp: number, initialContent: string) {
         this.line = line
-        this.start = new LineVersion(this, initialTimestamp, initialContent, {})
+        this.start = new LineVersion(this, initialTimestamp, initialContent)
         this.head = this.start
     }
 
@@ -77,14 +271,16 @@ class LineVersion {
     private previous: LineVersion | undefined
     private next: LineVersion | undefined
 
-    constructor(history: LineHistory, timestamp: number, content: string, relations: LineVersionRelation) {
+    constructor(history: LineHistory, timestamp: number, content: string, relations?: LineVersionRelation) {
         this.histoy = history
         this.timestamp = timestamp
         this.content = content
 
-        this.origin = relations.origin
-        this.previous = relations.previous
-        this.next = relations.next
+        if (relations) {
+            this.origin = relations.origin
+            this.previous = relations.previous
+            this.next = relations.next
+        }
     }
 }
 
