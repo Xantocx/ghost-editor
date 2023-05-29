55afb20deab6a4353bbe511fe781455656f0fbde
Maximilian Mayer <maximilian_mayer@gmx.net>
2023-05-29 11:42:05 +0200

    (hopfully) fixed resizing of error message properly

--- src/editor/ui/views/previews/p5js-preview.ts
+++ src/editor/ui/views/previews/p5js-preview.ts
@@ -1,4 +1,3 @@
-import { sleep } from "../../../utils/helpers";
 import { Disposable } from "../../../utils/types";
 import { uuid } from "../../../utils/uuid";
 import { CodePreview } from "./preview";
@@ -11,21 +10,22 @@ export class P5JSPreview extends CodePreview {
 
     private readonly uuid: string = uuid(16)
 
-    private readonly iframeContainer: HTMLDivElement
-    private iframe: HTMLIFrameElement
+    private readonly container: HTMLDivElement
+
+    private iframeVisible = false
+    private iframe:       HTMLIFrameElement
+    
     private hasErrorMessage = false
+    private errorMessage: HTMLDivElement
 
     private readonly errorMessageColor: string
 
+    private resizeObserver?: ResizeObserver
     private onResizeCallbacks: {(width: number, height: number, scaleFactor: number): void}[] = []
 
     private get id(): string { return `p5js-preview-${this.uuid}` }
 
     private get html(): string {
-
-        const contentWidth  = this.getContentWidth()
-        const contentHeight = this.getContentHeight()
-
         return `
             <!DOCTYPE html>
             <html lang="en">
@@ -43,20 +43,30 @@ export class P5JSPreview extends CodePreview {
             <body>
 
                 <script>
+                    let sentErrorState  = false
+                    let hasErrorMessage = false
+                    let errorMessage    = undefined
+                    let errorStack      = undefined
+
+                    function sendErrorMessage() {
+                        const iframe = window.parentIFrame
+                        if (hasErrorMessage && !sentErrorState && iframe) {
+                            iframe.sendMessage({ hasErrorMessage: true, message: errorMessage, stack: errorStack })
+                            sentErrorState = true
+                        }
+                    }
+
+                    function createErrorMessage(error) {
+                        hasErrorMessage = true
+                        errorMessage    = error.message
+                        errorStack      = error.stack
+
+                        sendErrorMessage()
+                    }
+
                     // setup config object of iFrameResizer
                     window.iFrameResizer = {
-                        onReady:   ()   => {
-                            window.parentIFrame?.sendMessage(document.getElementById("error-message") !== null)
-                        },
-                        onMessage: data => {
-                            console.log("TEST")
-                            console.log(data)
-                            const errorMessage = document.getElementById("error-message")
-                            if (errorMessage) {
-                                errorMessage.style.width  = data.width  + "px"
-                                errorMessage.style.height = data.height + "px"
-                            }
-                        }
+                        onReady: () => { sendErrorMessage() }
                     }
                 </script>
                 <script src="${P5JSPreview.iframeResizerScript}"></script>
@@ -85,29 +95,10 @@ export class P5JSPreview extends CodePreview {
                         for (let i = 0; i < p5Canvas.length; i++) { p5Canvas[i].remove() }
                     }
 
-                    function createErrorMessage(text) {
-                        const errorMessage = document.createElement("div")
-
-                        errorMessage.id = "error-message"
-                        errorMessage.setAttribute("data-iframe-width", "")
-                        errorMessage.innerText = text
-
-                        // the way maxHeight is used should probably be made more explicit
-                        errorMessage.style.display   = "inline-block"
-                        errorMessage.style.boxSizing = "border-box"
-                        errorMessage.style.width     = "${contentWidth}px"
-                        errorMessage.style.height    = "${contentHeight}px"
-                        errorMessage.style.padding   = "3px"
-                        errorMessage.style.color     = "${this.errorMessageColor}"
-                        errorMessage.style.border    = "1px solid ${this.errorMessageColor}"
-
-                        document.body.appendChild(errorMessage);
-                    }
-
                     const code = ${JSON.stringify(this.code)}
 
                     if (!code.includes("setup") || !code.includes("draw")) {
-                        createErrorMessage("Your code must include a 'setup' and a 'draw' function to be rendered in this P5JS preview.")
+                        createErrorMessage({ message: "Your code must include a 'setup' and a 'draw' function to be rendered in this P5JS preview.", stack: "" })
                     } else {
                         try {
                             eval(code)
@@ -121,7 +112,7 @@ export class P5JSPreview extends CodePreview {
                                         userSetup.call(this);
                                     } catch (error) {
                                         stopP5()
-                                        createErrorMessage("Message: " + error.message${contentHeight > 300 ? ' + "\\n\\nStack:\\n\\n" + error.stack' : "" })
+                                        createErrorMessage(error)
                                     }
                                 }
 
@@ -130,7 +121,7 @@ export class P5JSPreview extends CodePreview {
                                         userDraw.call(this);
                                     } catch (error) {
                                         stopP5()
-                                        createErrorMessage("Message: " + error.message${contentHeight > 300 ? ' + "\\n\\nStack:\\n\\n" + error.stack' : "" })
+                                        createErrorMessage(error)
                                     }
                                 }
 
@@ -140,11 +131,11 @@ export class P5JSPreview extends CodePreview {
                             } else {
                                 window.setup = undefined
                                 window.draw  = undefined
-                                createErrorMessage("Your code must include a 'setup' and a 'draw' function to be rendered in this P5JS preview.")
+                                createErrorMessage(error)
                             }
 
                         } catch (error) {
-                            createErrorMessage(error.message)
+                            createErrorMessage(error)
                         }
                     }
                 </script>
@@ -153,8 +144,12 @@ export class P5JSPreview extends CodePreview {
                     // workaround to allow the use of taggedElement for width calculation when sizing the iframe
                     window.addEventListener("load", event => {
                         const p5Canvas = document.getElementsByClassName("p5Canvas")
-                        for (let i = 0; i < p5Canvas.length; i++) {
-                            p5Canvas[i].setAttribute("data-iframe-width", "")
+                        if (p5Canvas.length > 0) {
+                            for (let i = 0; i < p5Canvas.length; i++) { p5Canvas[i].setAttribute("data-iframe-width", "") }
+                        } else {
+                            const sizingDiv = document.createElement("div")
+                            sizingDiv.setAttribute("data-iframe-width", "")
+                            document.body.appendChild(sizingDiv)
                         }
                     })
                 </script>
@@ -168,7 +163,7 @@ export class P5JSPreview extends CodePreview {
         return URL.createObjectURL(htmlBlob);
     }
 
-    public get style():         CSSStyleDeclaration { return this.iframeContainer.style }
+    public get style():         CSSStyleDeclaration { return this.container.style }
     public get iFrameResizer(): any | undefined     { return (this.iframe as any).iFrameResizer }
 
     private readonly padding:   number
@@ -182,40 +177,38 @@ export class P5JSPreview extends CodePreview {
         this.minHeight         = options?.minHeight         ? options.minHeight         : 50
         this.errorMessageColor = options?.errorMessageColor ? options.errorMessageColor : "black"
 
-        this.iframeContainer = document.createElement("div")
+        this.container = document.createElement("div")
         this.style.position  = "relative"
         this.style.boxSizing = "border-box"
         this.style.width     = "100%"
         this.style.height    = "100%"
-        this.style.margin    = "0 0"
-        this.style.overflow  = "hidden"
-        this.root.appendChild(this.iframeContainer)
+        this.style.padding   = `${this.padding}px`
+        this.style.margin    = "0"
+        this.root.appendChild(this.container)
 
+        // setup iframe
         this.iframe = document.createElement("iframe") as HTMLIFrameElement
         this.iframe.id = this.id
         this.iframe.frameBorder = "0"
-
-        // make sure preview is displayed with minimal unused space
-        this.iframe.style.position = "absolute"
+        this.iframe.style.position  = "absolute"
         this.iframe.style.top       = "50%"
         this.iframe.style.left      = "50%"
-        this.iframe.style.width     = "100%"
-        this.iframe.style.height    = "100%"
         this.iframe.style.transform = "translate(-50%, -50%)"
-
-        this.iframe.style.padding = "0 0"
-        this.iframe.style.margin  = "0 0"
+        this.iframe.style.padding = "0"
+        this.iframe.style.margin  = "0"
         this.iframe.style.border  = "none"
 
-        let loadHandler: () => void
-        loadHandler = () => {
-            this.iframe.removeEventListener("load", loadHandler)
-            this.setupResizing()
-            if (this.code) { this.render() }
-        }
+        // setup div for error message, should it be needed
+        this.errorMessage = document.createElement("div")
+        this.errorMessage.style.display   = "inline-block"
+        this.errorMessage.style.boxSizing = "border-box"
+        this.errorMessage.style.width     = "100%"
+        this.errorMessage.style.height    = "100%"
+        this.errorMessage.style.padding   = "3px"
+        this.errorMessage.style.color     = this.errorMessageColor
+        this.errorMessage.style.border    = `1px solid ${this.errorMessageColor}`
 
-        this.iframe.addEventListener("load", loadHandler)
-        this.iframeContainer.appendChild(this.iframe)
+        if (this.code) { this.render() }
     }
 
     private padValue(value: number): number {
@@ -227,85 +220,73 @@ export class P5JSPreview extends CodePreview {
     }
 
     private getContentWidth(): number {
-        return this.padValue(Math.max(parseFloat(window.getComputedStyle(this.iframeContainer).width), this.minWidth))
+        return this.padValue(Math.max(parseFloat(window.getComputedStyle(this.container).width), this.minWidth))
     }
 
     private getContentHeight(): number {
-        return this.padValue(Math.max(parseFloat(window.getComputedStyle(this.iframeContainer).height), this.minHeight))
+        return this.padValue(Math.max(parseFloat(window.getComputedStyle(this.container).height), this.minHeight))
     }
 
     private setupResizing(): void {
-        const onResize  = (data: any) => { this.resize(data.width, data.height) }
-        const onMessage = (data: any) => { 
-            this.hasErrorMessage = data.message
+        
+        const onResize = (data: any) => { this.resizeIFrame(data.width, data.height) }
+        const onError  = (data: any) => { 
 
+            const error = data.message
+
+            this.hasErrorMessage = error.hasErrorMessage
             if (this.hasErrorMessage) {
-                //this.iframe.style.top       = "auto"
-                //this.iframe.style.left      = "auto"
-                this.iframe.style.transform = "translate(-50%, -50%)"
-            }
+                this.hideIFrame()
 
-            this.resizeErrorMessage()
+                this.errorMessage.innerText = `Message: ${error.message}` + (this.getContentHeight() > 300 ? `\nStack:\n\n${error.stack}` : "")
+                this.container.appendChild(this.errorMessage)
+            }
         }
+
         this.iframe = iframeResize({ /*log: true,*/ 
                                         checkOrigin: ["file://"], 
                                         sizeWidth: true, 
                                         widthCalculationMethod: "taggedElement",
                                         tolerance: 20, // used to avoid recursive resizing loop over small inaccuracies in size
                                         onResized: onResize,
-                                        onMessage: onMessage
+                                        onMessage: onError
                                    }, `#${this.id}`)[0]
 
-        // TODO: replace with size observer
-        window.addEventListener("resize", (event) => {
-            const width  = parseFloat(this.iframe.style.width)
-            const height = parseFloat(this.iframe.style.height)
-            this.resize(width, height)
+        this.resizeObserver = new ResizeObserver((entries) => {
+            if (!this.hasErrorMessage) {
+                const width  = parseFloat(this.iframe.style.width)
+                const height = parseFloat(this.iframe.style.height)
+                this.resizeIFrame(width, height)
+            }
         })
+        this.resizeObserver.observe(this.container)
     }
 
-    private resizeErrorMessage(): void {
-        const contentWidth  = this.getContentWidth()
-        const contentHeight = this.getContentHeight()
+    private resizeIFrame(iframeWidth: number, iframeHeight: number): void {
+        // includes padding
+        const scaleFactor = Math.min(this.getContentWidth() / iframeWidth, this.getContentHeight() / iframeHeight)
 
-        console.log(contentWidth)
+        this.iframe.style.transformOrigin = "top left"
+        this.iframe.style.transform       = `scale(${scaleFactor}) translate(-50%, -50%)` // translate is used to center element vertically
 
+        const contentWidth  = iframeWidth  * scaleFactor
+        const contentHeight = iframeHeight * scaleFactor
+
+        /*
+        console.log("CURRENT")
+        console.log(iframeWidth)
+        console.log(iframeHeight)
+        console.log("DESIRED")
+        console.log(displayWidth)
+        console.log(displayHeight)
+        console.log("---------------------")
+        */
+
+        // these values are padded, meaning they assume a padding will be added around them
         const displayWidth  = this.unpadValue(contentWidth)
         const displayHeight = this.unpadValue(contentHeight)
 
-        this.iFrameResizer?.sendMessage( { width: contentWidth, height: contentHeight })
-        this.onResizeCallbacks.forEach(callback => callback(displayWidth, displayHeight, 1))
-    }
-
-    private resize(iframeWidth: number, iframeHeight: number): void {
-        if (this.hasErrorMessage) { 
-            this.resizeErrorMessage()
-        } else {
-            // includes padding
-            const scaleFactor = Math.min(this.getContentWidth() / iframeWidth, this.getContentHeight() / iframeHeight)
-
-            this.iframe.style.transformOrigin = "top left"
-            this.iframe.style.transform       = `scale(${scaleFactor}) translate(-50%, -50%)` // translate is used to center element vertically
-
-            const contentWidth  = iframeWidth  * scaleFactor
-            const contentHeight = iframeHeight * scaleFactor
-
-            /*
-            console.log("CURRENT")
-            console.log(iframeWidth)
-            console.log(iframeHeight)
-            console.log("DESIRED")
-            console.log(displayWidth)
-            console.log(displayHeight)
-            console.log("---------------------")
-            */
-
-            // these values are padded, meaning they assume a padding will be added around them
-            const displayWidth  = this.unpadValue(contentWidth)
-            const displayHeight = this.unpadValue(contentHeight)
-
-            this.onResizeCallbacks.forEach(callback => callback(displayWidth, displayHeight, scaleFactor))
-        }
+        this.onResizeCallbacks.forEach(callback => callback(displayWidth, displayHeight, scaleFactor))
     }
 
     public onResize(callback: (width: number, height: number, scaleFactor: number) => void): Disposable {
@@ -321,12 +302,46 @@ export class P5JSPreview extends CodePreview {
         })
     }
 
+    private hideIFrame(): void {
+        if (this.iframeVisible) {
+            this.resizeObserver?.disconnect()
+            this.iFrameResizer?.close()
+            this.iframe.remove()
+
+            this.resizeObserver = undefined
+            this.iframeVisible  = false
+        }
+    }
+
+    public showIFrame(): void {
+        if (!this.iframeVisible) {
+            let loadHandler: () => void
+            loadHandler = () => {
+                this.iframe.removeEventListener("load", loadHandler)
+                this.setupResizing()
+            }
+
+            this.iframe.addEventListener("load", loadHandler)
+            this.container.appendChild(this.iframe)
+
+            this.iframeVisible = true
+        }
+    }
+
     public override async render(): Promise<void> {
         this.iframe.src = this.htmlUrl
+
+        if (this.hasErrorMessage) {
+            this.errorMessage.remove()
+            this.hasErrorMessage = false
+        }
+
+        this.showIFrame()
     }
 
     public override remove(): void {
-        this.iFrameResizer?.close()
+        this.showIFrame()
+        this.errorMessage.remove()
         super.remove()
     }
 }
